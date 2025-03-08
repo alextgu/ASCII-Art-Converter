c0f5100bba316b949d2c5d6a5c6e1858d396d40b
Alexander Gu <agu@Alexanders-MacBook-Air.local>
2025-03-08 17:19:44 -0500

    reworked system

--- ascii_converter.py
+++ ascii_converter.py
@@ -1,8 +1,7 @@
 from PIL import Image
-from colorama import Fore, Style
-import os
+from colorama import Fore, Style, init
 
-ASCII_order = "`^\",:;Il!i~+_-?][}{1)(|/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$"
+ASCII_ORDER = "`^\",:;Il!i~+_-?][}{1)(|/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$"
 MAX_PIXEL_VALUE = 255
 
 def get_pixel_matrix(height, im):
@@ -16,76 +15,36 @@ def get_intensity_matrix(matrix, algo='avg'):
         temp_row = []
         for i in row:
             if algo == "avg":
-                temp_row.append(round((i[0]+i[1]+i[2])/3,2))
+                temp_row.append(round((i[0]+i[1]+i[2])/3, 2))
             elif algo == "max min":
-                temp_row.append(round(((i[0]+i[2])/2),2))
-        
+                temp_row.append(round(((i[0]+i[2])/2), 2))
             elif algo == "luminosity":
-                temp_row.append(round(0.21*i[0] + 0.72*i[1] + 0.07*i[2],2))
+                temp_row.append(round(0.21*i[0] + 0.72*i[1] + 0.07*i[2], 2))
             else:
-                raise Exception('Undefined algorithm. Please try "avg", "max min" or "luminosity"')
+                raise Exception('Undefined algorithm. Use "avg", "max min", or "luminosity".')
         intensity_matrix.append(temp_row)
     return intensity_matrix
 
 def get_char_matrix(intensity_matrix):
     char_matrix = []
-    max_pixel = max(map(max, intensity_matrix)) # Darkest pixel
-    min_pixel = min(map(min, intensity_matrix)) # Lightest pixel
+    max_pixel = max(map(max, intensity_matrix))  
+    min_pixel = min(map(min, intensity_matrix))  
     for row in intensity_matrix:
-        temp_row = []
-        for i in row:
-            rescaled_pixel = max_pixel*(i-min_pixel)/float(max_pixel - min_pixel)
-            temp_row.append(rescaled_pixel)
+        temp_row = [(max_pixel * (i - min_pixel) / float(max_pixel - min_pixel)) for i in row]
         char_matrix.append(temp_row)
     return char_matrix
 
-
 def get_ASCII(intensity_matrix, ascii_chars):
-    ascii_matrix = []
-    for row in intensity_matrix:
-        temp_row = []
-        for i in row:
-            temp_row.append(ascii_chars[int(i/MAX_PIXEL_VALUE * len(ascii_chars)) - 1])
-        ascii_matrix.append(temp_row)
-    return ascii_matrix
+    return [[ascii_chars[int(i / MAX_PIXEL_VALUE * len(ascii_chars)) - 1] for i in row] for row in intensity_matrix]
 
-def save_ascii_image(input_path, output_path, text_colour):
+def generate_ascii_art(input_path, text_colour=Fore.GREEN):  # Set a default text color
     im = Image.open(input_path)
     pixels = get_pixel_matrix(1000, im)
 
     intensity_matrix = get_intensity_matrix(pixels, "avg")
     intensity_matrix = get_char_matrix(intensity_matrix)
 
-    ascii_matrix = get_ASCII(intensity_matrix, ASCII_order)
+    ascii_matrix = get_ASCII(intensity_matrix, ASCII_ORDER)
 
-    with open(output_path, 'w') as f:
-        for row in ascii_matrix:
-            line = "".join(row)
-            f.write(text_colour + line + "\n")  
-        f.write(Style.RESET_ALL) 
-
-    return output_path 
-
-
-"""
-def print_ascii_matrix(ascii_matrix, text_color):
-    for row in ascii_matrix:
-        line = "".join(row)
-        print(text_color + line)
-    print(Style.RESET_ALL)
-
-
-path = "/Users/agu/Desktop/ASCII_Art/static/files/download.jpeg"
-
-im = Image.open(path)
-pixels = get_pixel_matrix(1000,im)
-
-
-intensity_matrix = get_intensity_matrix(pixels, "avg")
-intensity_matrix = get_char_matrix(intensity_matrix)
-
-ascii_matrix = get_ASCII(intensity_matrix, ASCII_order)
-print_ascii_matrix(ascii_matrix, Fore.BLACK)
-
-
-"""
+    ascii_output = "\n".join(["".join(row) for row in ascii_matrix])
+    return text_colour + ascii_output + Style.RESET_ALL
--- main.py
+++ main.py
@@ -1,50 +1,60 @@
-from flask import Flask, render_template, send_file
+from flask import Flask, render_template, request, send_file, redirect, url_for
 from flask_wtf import FlaskForm
 from wtforms import FileField, SubmitField
 from werkzeug.utils import secure_filename
 import os
 from wtforms.validators import InputRequired
-from ascii_converter import save_ascii_image
-from colorama import Fore
+from ascii_converter import generate_ascii_art
+import io
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'supersecretkey'
 app.config['UPLOAD_FOLDER'] = 'static/files'
-app.config['OUTPUT_FOLDER'] = 'static/output_files'
 
 os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
-os.makedirs(app.config['OUTPUT_FOLDER'], exist_ok=True)
 
 class UploadFileForm(FlaskForm):
-    file = FileField("File", validators=[InputRequired()])
-    submit = SubmitField("Upload File")
+    file = FileField("Upload Image", validators=[InputRequired()])
+    submit = SubmitField("Convert to ASCII")
 
-@app.route('/', methods=['GET',"POST"])
-@app.route('/home', methods=['GET',"POST"])
+@app.route('/', methods=['GET', 'POST'])
 def home():
     form = UploadFileForm()
+    
     if form.validate_on_submit():
         file = form.file.data
-
-        file_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), app.config['UPLOAD_FOLDER'], secure_filename(file.filename))
+        filename = secure_filename(file.filename)
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
         file.save(file_path)
         
-        output_file_path = os.path.join(app.config['OUTPUT_FOLDER'], f"{secure_filename(file.filename)}.txt")
-        save_ascii_image(file_path, output_file_path, text_colour=Fore.GREEN)
+        # Convert the image to ASCII art
+        ascii_art = generate_ascii_art(file_path)
 
-        return render_template('ascii.html', ascii_art=output_file_path, filename=f"{secure_filename(file.filename)}.txt")
-    
+        ascii_file = io.BytesIO()
+        ascii_file.write(ascii_art.encode('utf-8'))
+        ascii_file.seek(0)
+
+        return redirect(url_for('download_page', filename=filename))
+
     return render_template('index.html', form=form)
 
 @app.route('/download/<filename>')
-def download_ascii(filename):
-    filepath = os.path.join(app.config['OUTPUT_FOLDER'], filename)
-    return send_file(filepath, as_attachment=True)
+def download_page(filename):
+    # Generate ASCII art file
+    file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    ascii_art = generate_ascii_art(file_path)
+
+    # Create an in-memory file to be downloaded
+    ascii_file = io.BytesIO()
+    ascii_file.write(ascii_art.encode('utf-8'))
+    ascii_file.seek(0)
+
+    return send_file(
+        ascii_file,
+        as_attachment=True,
+        download_name=f"{filename}.txt",
+        mimetype="text/plain"
+    )
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-
-        
-
