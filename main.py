842679ca1bb483fbe625945ac8fd0b6642cb76de
Alexander Gu <agu@Alexanders-MacBook-Air.local>
2025-03-08 18:15:16 -0500

    created simple frontend

--- main.py
+++ main.py
@@ -10,51 +10,43 @@ import io
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'supersecretkey'
 app.config['UPLOAD_FOLDER'] = 'static/files'
+app.config['OUTPUT_FOLDER'] = 'static/output_files'
 
 os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
+os.makedirs(app.config['OUTPUT_FOLDER'], exist_ok=True)
 
 class UploadFileForm(FlaskForm):
-    file = FileField("Upload Image", validators=[InputRequired()])
+    file = FileField("Upload Image:", validators=[InputRequired()])
     submit = SubmitField("Convert to ASCII")
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
     form = UploadFileForm()
-    
+
     if form.validate_on_submit():
         file = form.file.data
         filename = secure_filename(file.filename)
         file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
         file.save(file_path)
-        
+
         # Convert the image to ASCII art
         ascii_art = generate_ascii_art(file_path)
+        ascii_filename = f"{filename.split('.')[0]}.txt"  # Using the original filename without extension
 
-        ascii_file = io.BytesIO()
-        ascii_file.write(ascii_art.encode('utf-8'))
-        ascii_file.seek(0)
+        # Save ASCII art as a text file
+        output_file_path = os.path.join(app.config['OUTPUT_FOLDER'], ascii_filename)
+        with open(output_file_path, 'w', encoding='utf-8') as f:
+            f.write(ascii_art)
 
-        return redirect(url_for('download_page', filename=filename))
+        # Redirect to the download page with the filename
+        return redirect(url_for('download_page', filename=ascii_filename))
 
     return render_template('index.html', form=form)
 
 @app.route('/download/<filename>')
 def download_page(filename):
-    # Generate ASCII art file
-    file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-    ascii_art = generate_ascii_art(file_path)
-
-    # Create an in-memory file to be downloaded
-    ascii_file = io.BytesIO()
-    ascii_file.write(ascii_art.encode('utf-8'))
-    ascii_file.seek(0)
-
-    return send_file(
-        ascii_file,
-        as_attachment=True,
-        download_name=f"{filename}.txt",
-        mimetype="text/plain"
-    )
+    filepath = os.path.join(app.config['OUTPUT_FOLDER'], filename)
+    return send_file(filepath, as_attachment=True, download_name=filename, mimetype="text/plain")
 
 if __name__ == '__main__':
     app.run(debug=True)
